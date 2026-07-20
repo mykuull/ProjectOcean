b21c46c359e635c460eac0aae36fda2f4e0450bb
mang-s <miguelang1120@gmail.com>
2026-07-20 17:07:03 +0200

    folder reorganization & minor changes
    reorganized the scenes folder and minor changes to the balance scene on the chronometer_test scene
    also deleted some useless sounds

--- ocean/scripts/chronometer_placeholder.gd
+++ ocean/scripts/chronometer_placeholder.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 func _on_back_pressed() -> void:
 	
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") # Scene to go back to
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn") # Scene to go back to
