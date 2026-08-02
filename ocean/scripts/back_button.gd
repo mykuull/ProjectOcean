f320662da08356f22d6758c68ccffa5f1984249f
mang-s <miguelang1120@gmail.com>
2026-08-02 21:05:27 +0200

    extra var missed in last update

--- ocean/scripts/back_button.gd
+++ ocean/scripts/back_button.gd
@@ -1,7 +1,5 @@
 extends Button
 
-@onready var sfx_back_press: AudioStreamPlayer = $SFXPressBack
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
