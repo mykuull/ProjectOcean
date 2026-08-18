fd9a96723acaddfc0b0fe46e554950b7287a5ffd
mang-s <miguelang1120@gmail.com>
2026-08-18 03:34:44 +0200

    funcionalidad a volumen en los settings
    Se ha añadido funcionalidad al volumen en los settings, guardando los valores en el archivo de guardado como se hace con el balance.

--- ocean/scripts/settings.gd
+++ ocean/scripts/settings.gd
@@ -1,14 +1,17 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
+@onready var volume_selector = %Volume
+
 func _ready() -> void:
-	pass # Replace with function body.
 
+	volume_selector.set_valor_numerico(SaveLoad.contents.volume) # le decimos que muestre el valor guardado
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_back_pressed() -> void:
 	
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn") # Scene to go back to
+
+func _on_volume_numeric_value_changed(new_value: int) -> void:
+	SaveLoad.set_volume(new_value)
